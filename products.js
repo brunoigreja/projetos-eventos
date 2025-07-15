39b3cc26c6085038478b1fc9b20ec918b3d7e3c7
brunoigreja <igrejasanto044@gmail.com>
2025-07-15 14:33:43 -0300

    projeto-js-devClub

---
+++ products.js
@@ -0,0 +1,15 @@
+const menuOptions = [
+
+{ name: 'X-Salada', price: 30.99, vegan: false, src: './img/xsalada.jpeg' },
+
+{ name: 'X-Bacon', price: 34.99, vegan: false, src: './img/xbacon.png' },
+
+{ name: 'X-Bacon Egg', price: 39.59, vegan: false, src: './img/bacon-egg.png' },
+
+{ name: 'Monstruoso', price: 50.99, vegan: false, src: './img/monstruoso.png' },
+
+{ name: 'Big Vegano', price: 55.89, vegan: true, src: './img/xvegan.png' },
+
+{ name: 'X-Vegan', price: 45.49, vegan: true, src: './img/monstruoso-vegan.png' },
+
+]
